03a9513aee676edb7e7b80937cae33d9d9e8b488
Aakash Kumar <groot@localhost.localdomain>
2020-05-05 06:04:21 +0530

    OSINT_complete

--- weaponhex.py
+++ weaponhex.py
@@ -246,8 +246,9 @@ def web():
 def osint():
     print("""
         \033[32m[1]  \033[36mSocial Scan
-        \033[32m[1]  \033[36mSome Tools
-        \033[32m[1]  \033[36mBack
+        \033[32m[2]  \033[36mDataSploit
+        \033[32m[3]  \033[36mReconSpider
+        \033[32m[99]  \033[36mBack
         """)
     choice = input("\033[35mEnter your choice =>>\033[32m")
     if choice == "1":
@@ -255,7 +256,10 @@ def osint():
         soscan()
     elif choice == "2":
         clearScr()
-        mrtools()
+        dtsploit()
+    elif choice == "3":
+        clearScr()
+        reconspider()
     elif choice == "99":
         menu()
     else :
@@ -265,6 +269,42 @@ def soscan():
     print("\033[32mSocial Scan Tool is a tool which scans username on most of the popular social media websites.")
     usname = input("\033[35mEnter username =>>\033[32m")
     os.system("./socialscan.sh " + usname + " -fu")
+    
+def dtsploit():
+    print("An OSINT framework to perform various recon techniques on Companies, People, Phone, Number, \nBitcoin Addresses, etc aggregate all the raw data, and give data in multiple formats.")
+    print("""
+        \033[32m[1]   \033[36mInstall
+        \033[32m[99]  \033[36mBack
+        """)
+    dtchoice = input("\033[35mEnter your choice =>>\033[32m")
+    if dtchoice == "1":
+        os.system("sudo git clone https://github.com/DataSploit/datasploit.git")
+        os.system("cd datasploit && pip install --upgrade --force-reinstall -r requirements.txt")
+        print("\033[32m[+]  \033[33mDataSploit Installed Successfully.")
+    elif dtchoice == "99":
+        osint()
+    else:
+        menu()
+        
+def reconspider():
+    print("ReconSpider is most Advanced Open Source Intelligence (OSINT) Framework for scanning IP Address, \nEmails, Websites, Organizations and find out information from different sources.")
+    print("""
+        \033[32m[1]   \033[36mInstall
+        \033[32m[2]   \033[36mRun
+        \033[32m[99]  \033[36mBack
+        """)
+    rspidchoice = input("\033[35mEnter your choice =>>\033[32m")
+    if rspidchoice == "1":
+        os.system("sudo git clone https://github.com/bhavsec/reconspider.git")
+        os.system("sudo apt install python3 python3-pip && cd reconspider && sudo python3 setup.py install")
+        print("\033[32m[+]  \033[33mReconSpider Installed Successfully.")
+    elif rspidchoice == "2":
+        os.system("cd reconspider && python3 reconspider.py")
+    elif rspidchoice == "99":
+        osint()
+    else :
+        menu()
+    
 
 def clearScr():
     if system() == 'Linux':
@@ -276,13 +316,13 @@ if __name__ == "__main__":
     try:
         if system() == 'Linux':
             if path.exists("/home/"):
-                os.chdir("/home/groot/Desktop")
+                os.chdir("/home")
                 if os.path.isdir('WeaponHEX'):
-                    os.chdir("/home/groot/Desktop/WeaponHEX/")
+                    os.chdir("/home/WeaponHEX/")
                     menu()
                 else :
                     os.system("mkdir WeaponHEX")
-                    os.chdir("/home/groot/Desktop/WeaponHEX/")
+                    os.chdir("/home/WeaponHEX/")
                     menu()
         elif path.exists('/data'):
             os.chdir("data/data/com.termux/files/home/")
